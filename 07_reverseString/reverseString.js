20f73f57038f69585b0d8166588010e3efc1daae
Kushaal Kankane <kushaalkankane12345@gmail.com>
2025-06-11 18:09:01 +0000

    Completed second set of exercise

--- 07_reverseString/reverseString.js
+++ 07_reverseString/reverseString.js
@@ -1,5 +1,12 @@
-const reverseString = function() {
+const reverseString = function(string) {
+    let ans = "";
 
+    for(let i = string.length;i>=0;i--)
+    {
+        ans+= string.charAt(i);
+    }
+
+    return ans;
 };
 
 // Do not edit below this line
--- 08_removeFromArray/removeFromArray.js
+++ 08_removeFromArray/removeFromArray.js
@@ -1,5 +1,21 @@
-const removeFromArray = function() {
+const removeFromArray = function(arr, ...nums) {
+
+    for(let num of nums)
+    {
+        for(let i =0;i<arr.length;i++)
+        {
+            if(arr[i] === num)
+            {
+                arr.splice(i,1);
+                i--;
+            }
+        }
+    }
+    
+
+    return arr;
 };
+
 
 // Do not edit below this line
 module.exports = removeFromArray;
